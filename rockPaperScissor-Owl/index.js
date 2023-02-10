2a3dacd6db67b85cbcb53d82fb1ef1f50074f4b5
Deependra Solanki <deso@odoo.com>
2023-02-10 18:40:12 +0530

    Implemented useState and Reactivity

--- rockPaperScissor-Owl/index.js
+++ rockPaperScissor-Owl/index.js
@@ -1,44 +1,88 @@
-const { Component ,mount, xml} = owl;
+const { Component ,mount, xml, props, useState, useEnv, reactive} = owl;
 const computersOptions = ["Rock", "Paper", "Scissor"];
+
+const connector = () =>{
+    const env = useEnv();
+    return useState(env.store)
+}
+
+class updates{
+    playerScore = 0;
+    computerScore = 0;
+    computerChoiceSpan = ""
+    humanChoiceSpan = ""
+    result = ""
+    updatePlayerScore(){
+        this.playerScore++;
+    }
+    getPlayerScore(){
+        return this.playerScore;
+    }
+    updateComputerScore(){
+        this.computerScore++;
+    }
+    getComputerScore(){
+        return this.computerScore;
+    }
+    getComputerChoiceSpan(){
+        return this.computerChoiceSpan;
+    }
+    getHumanChoiceSpan(){
+        return this.humanChoiceSpan;
+    }
+    getResult(){
+        return this.result;
+    }
+}
+
+const updateGame = () => {
+    return reactive(new updates);
+}
 
 class ComputerComponent extends Component{
     static template = xml `
         <div class="score">
             <h3><t t-esc="props.user" /></h3>
             <div class="playersDiv">
-                <t t-set="playerSpan" t-value="props.playerSpan"/>
-                <span id="computerChoiceSpan"></span>
-                <span id="computerChoiceIcon"></span>
+                <span id="computerChoiceSpan"><t t-esc="this.changes.getComputerChoiceSpan()" /></span>
             </div>
         </div>
     `
+    setup(){
+        this.changes = connector();
+    }
     static props = ["user"];
-
 }
 class PlayerComponent extends Component{
     static template = xml `
         <div class="score"> 
             <h3><t t-esc="props.user" /></h3>
             <div class="playersDiv">
-                <span id="humanChoiceSpan"></span>
-                <span id="humanChoiceIcon"></span>
+                <span id="humanChoiceSpan"><t t-esc="this.changes.getHumanChoiceSpan()" /></span>
             </div>
         </div>
     `
+    setup(){
+        this.changes = connector();
+    }
     static props = ["user"]
 }
 class Score extends Component{
     static template = xml`
         <div class="d-flex container">
             <div>
-                Player <span id="humanScore">0</span> :
-                <span id="computerScore">0</span> Computer
+                Player <span id="humanScore"><t t-esc="this.changes.getPlayerScore()" /></span> :
+                <span id="computerScore"><t t-esc="this.changes.getComputerScore()" /></span> Computer
                 <div class="resultDiv">
-                    Result: <span id="result"></span>
+                    Result: <span id="result"><t t-esc="this.changes.getResult()" /></span>
                 </div>
             </div>
         </div>
     `;
+
+    setup(){
+        this.changes = connector();
+    }
 }
 class Buttons extends Component{
     static template = xml`
@@ -48,71 +92,49 @@ class Buttons extends Component{
             <button value="Scissor" t-on-click="scissorBtn">Scissor</button>
         </div>
     `
-    
+    setup(){
+        this.changes = connector();
+    }
     rockBtn(){
         let computerChoice = Math.floor(Math.random() * 3);
-        computerChoiceSpan.innerText = computersOptions[computerChoice];
-        humanChoiceIcon.innerHTML =
-            '<i class="fa-solid fa-hand-back-fist fa-5x"></i>';
-        humanChoiceSpan.innerText = "Rock";
+        this.changes.computerChoiceSpan = computersOptions[computerChoice];
+        this.changes.humanChoiceSpan = "Rock";
         if (computerChoice === 0) {
-            result.innerText = "Draw";
-            computerChoiceIcon.innerHTML =
-                '<i class="fa-solid fa-hand-back-fist fa-5x"></i>';
+            this.changes.result = "Draw";
         } else if (computerChoice === 1) {
-            result.innerText = "Computer Won!!!";
-            computerChoiceIcon.innerHTML =
-                '<i class="fa-solid fa-hand fa-5x"></i>';
-            computerScore.innerText++;
+            this.changes.result = "Computer Won!!!";
+            this.changes.updateComputerScore();
         } else {
-            result.innerText = "You Won!!!";
-            computerChoiceIcon.innerHTML =
-                '<i class="fa-solid fa-hand-scissors fa-5x"></i>';
-            humanScore.innerText++;
+            this.changes.result = "You Won!!!";
+            this.changes.updatePlayerScore();
         }
     }
     paperBtn(){
         let computerChoice = Math.floor(Math.random() * 3);
-        computerChoiceSpan.innerText = computersOptions[computerChoice];
-        humanChoiceIcon.innerHTML = '<i class="fa-solid fa-hand fa-5x"></i>';
-        humanChoiceSpan.innerText = "Paper";
+        this.changes.computerChoiceSpan = computersOptions[computerChoice];
+        this.changes.humanChoiceSpan = "Paper";
         if (computerChoice === 0) {
-            result.innerText = "You Won!!!";
-            computerChoiceIcon.innerHTML =
-                '<i class="fa-solid fa-hand-back-fist fa-5x"></i>';
-            humanScore.innerText++;
+            this.changes.result = "You Won!!!";
+            this.changes.updatePlayerScore();
         } else if (computerChoice === 1) {
-            computerChoiceIcon.innerHTML =
-                '<i class="fa-solid fa-hand fa-5x"></i>';
-            result.innerText = "Draw";
+            this.changes.result = "Draw";
         } else {
-            result.innerText = "Computer Won!!!";
-            computerChoiceIcon.innerHTML =
-                '<i class="fa-solid fa-hand-scissors fa-5x"></i>';
-            computerScore.innerText++;
+            this.changes.result = "Computer Won!!!";
+            this.changes.updateComputerScore();
         }
     }
     scissorBtn(){
         let computerChoice = Math.floor(Math.random() * 3);
-        computerChoiceSpan.innerText = computersOptions[computerChoice];
-        humanChoiceIcon.innerHTML =
-            '<i class="fa-solid fa-hand-scissors fa-5x"></i>';
-        humanChoiceSpan.innerText = "Scissor";
+        this.changes.computerChoiceSpan = computersOptions[computerChoice];
+        this.changes.humanChoiceSpan = "Scissor";
         if (computerChoice === 0) {
-            result.innerText = "Computer Won!!!";
-            computerChoiceIcon.innerHTML =
-                '<i class="fa-solid fa-hand-back-fist fa-5x"></i>';
-
-            computerScore.innerText++;
+            this.changes.result = "Computer Won!!!";
+            this.changes.updateComputerScore();
         } else if (computerChoice === 1) {
-            result.innerText = "You Won!!!";
-            computerChoiceIcon.innerHTML =
-                '<i class="fa-solid fa-hand fa-5x"></i>';
-            humanScore.innerText++;
+            this.changes.result = "You Won!!!";
+            this.changes.updatePlayerScore();
         } else {
-            result.innerText = "Draw";
-            computerChoiceIcon.innerHTML =
-                '<i class="fa-solid fa-hand-scissors fa-5x"></i>';
+            this.changes.result = "Draw";
         }
     }
 }
@@ -122,24 +144,27 @@ class App extends Component{
         <div>
             <div class="outerDiv">
                 <ComputerComponent user="'Computer'"/>
-                <Score count="abc"/>
+                <Score/>
                 <PlayerComponent user="'Player'"/>
                 <Buttons />
             </div>
             <button t-on-click="resetBtn" id="resetBtn">Reset</button>
         </div>
     `
+    setup(){
+        this.changes = connector();
+    }
+
     resetBtn(){
-        humanChoiceSpan.innerText = "";
-        computerChoiceSpan.innerText = "";
-        humanScore.innerText = 0;
-        computerScore.innerText = 0;
-        result.innerText = "";
-        humanChoiceIcon.innerHTML = "";
-        computerChoiceIcon.innerHTML = "";
+        this.changes.humanChoiceSpan = "";
+        this.changes.computerChoiceSpan = "";
+        this.changes.playerScore = 0;
+        this.changes.computerScore = 0;
+        this.changes.result = "";
     }
     static components = {Buttons, Score, ComputerComponent, PlayerComponent};
 }
 
+const env = {store: updateGame()};
 
-mount (App, document.body)
+mount (App, document.body, {dev: true, env})
